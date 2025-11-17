9640b77dd7b7a12589d4dc7233e8cb4cc02845c9
EmilioRoma <emilioroma2212@gmail.com>
2025-11-16 23:53:20 -0600

    add: Rutas nuevas en la API para modificar y eliminar clientes proporcionadas por Sandy Chavelas.

--- sgst-server/app/models/modelo_clientes.py
+++ sgst-server/app/models/modelo_clientes.py
@@ -8,3 +8,13 @@ class DataCrearCliente(BaseModel):
     telefono_cliente: str
     direccion_cliente: str
     notas_cliente: str
+    
+class DataModificarCliente(BaseModel):
+    id_cliente: int
+    id_taller: int
+    nombre_cliente: str
+    apellidos_cliente: str
+    correo_cliente: str
+    telefono_cliente: str
+    direccion_cliente: str
+    notas_cliente: str
--- sgst-server/app/routes/clientes.py
+++ sgst-server/app/routes/clientes.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, status, Depends, HTTPException
 from app.auth.dependencies import verify_token
 from app.core.database import get_connection
 from mysql.connector import Error
-from app.models.modelo_clientes import DataCrearCliente
+from app.models.modelo_clientes import DataCrearCliente, DataModificarCliente
 
 router = APIRouter(
     prefix="/clientes",
@@ -84,6 +84,87 @@ def crear_cliente(data_cliente: DataCrearCliente, usuario=Depends(verify_token))
     except Error as err:
         print(f"Error en /crear_cliente: {err}")
         raise HTTPException(status_code=500, detail={"error": "Error interno del servidor"})
+    finally:
+        connection.close()
+        cursor.close()
+
+@router.put("/modificar_cliente", status_code=status.HTTP_200_OK)
+def modificar_cliente(data_cliente: DataModificarCliente, usuario=Depends(verify_token)):
+    try:
+        connection = get_connection()
+        cursor = connection.cursor(dictionary=True)
+        
+        cursor.execute("""
+            SELECT 'correo' as tipo FROM clientes 
+            WHERE correo_cliente = %s AND id_taller = %s AND id_cliente != %s
+            UNION
+            SELECT 'telefono' as tipo FROM clientes 
+            WHERE telefono_cliente = %s AND id_taller = %s AND id_cliente != %s
+        """, (
+            data_cliente.correo_cliente, data_cliente.id_taller, data_cliente.id_cliente,
+            data_cliente.telefono_cliente, data_cliente.id_taller, data_cliente.id_cliente
+            )
+        )
+
+        resultados = cursor.fetchall()
+        errores = [fila[0] for fila in resultados]
+        
+        if 'correo' in errores:
+            raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail={"error": "Ya existe un cliente con el correo ingresado"})
+        if 'telefono' in errores:
+            raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail={"error": "Ya existe un cliente con el telefono ingresado"})
+        
+        cursor.execute(""""
+            UPDATE 
+                clientes 
+            SET 
+                nombre_cliente = %s, 
+                apellidos_cliente = %s, 
+                correo_cliente = %s, 
+                telefono_cliente = %s, 
+                direccion_cliente = %s, 
+                notas_cliente = %s
+            WHERE
+                id_cliente = %s
+        """, (
+            data_cliente.nombre_cliente,
+            data_cliente.apellidos_cliente,
+            data_cliente.correo_cliente,
+            data_cliente.telefono_cliente,
+            data_cliente.direccion_cliente,
+            data_cliente.notas_cliente
+        ))
+        
+        if cursor.rowcount == 0:
+            raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail={"error": "Hubo un error al modificar al cliente"})
+        
+        connection.commit()
+        
+        return {"message": "El cliente ha sido modificado correctamente"}
+    except Error as err:
+        print(f"Error interno: {err}")
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail={"error": "Error interno en el servidor"})
+    finally:
+        connection.close()
+        cursor.close()
+
+@router.delete("/eliminar_cliente", status_code=status.HTTP_200_OK)
+def eliminar_cliente(id_cliente: int, usuario=Depends(verify_token)):
+    try:
+        connection = get_connection()
+        cursor = connection.cursor(dictionary=True)
+        
+        cursor.execute("UPDATE clientes SET activo = 0 WHERE id_cliente = %s", (id_cliente, ))
+        
+        if cursor.rowcount == 0:
+            raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail={"error": "Hubo un error al eliminar el cliente"})
+        
+        connection.commit()
+        
+        return {"message", "El usuario ha sido eliminado correctamente"}
+    except Error as err:
+        print(f"Error interno: {err}")
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail={"error": "Error interno en el servidor"})
     finally:
         connection.close()
         cursor.close()
